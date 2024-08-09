a7a36727a38b0926092a5b11ea30332f518f0c29
Farrhouq <farouqimoro@gmail.com>
2024-08-09 23:12:53 +0000

    edit pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Pascal triangle function"""
+"""Pascal Triangle"""
+
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of a given number"""
+    """This is the pascal triangle generator"""
+    res = []
     if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    
-    return triangle
+        return res
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if i == 0:
+                row.append(1)
+            elif i == 1:
+                row.append(1)
+            else:
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(res[i-1][j-1] + res[i-1][j])
+        res.append(row)
+    return res
